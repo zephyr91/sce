0346003d6b87d826b847f5d99193186ee751d86c
David Henrique Langbajn <zephyr91@gmail.com>
2013-11-04 14:02:31 -0200

    Correção da merda do cache do twig para $_SERVER['DOCUMENT_ROOT']/cache_twig.

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,6 @@
 <?php
 
+$cache = $_SERVER['DOCUMENT_ROOT'] . '/cache_twig';
 require_once  __DIR__ . '/vendor/autoload.php';
 require  __DIR__ . '/pChart2.1.3/class/pDraw.class.php';
 require  __DIR__ . '/pChart2.1.3/class/pImage.class.php';
@@ -7,5 +8,5 @@ require  __DIR__ . '/pChart2.1.3/class/pData.class.php';
 
 $loader = new Twig_Loader_Filesystem( __DIR__ . '/html');
 $twig = new Twig_Environment($loader, array(
-    'cache' =>  'cache_twig',
+    'cache' =>  $cache,
 ));
